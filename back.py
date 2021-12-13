3fac5917efddf5a273ff9d09e4a6b069a673cfbc
Denis Borisov <mai.borisov.denis@gmail.com>
2021-12-13 19:32:10 +0300

    добавил правильный ввод с клавиатуры

--- back.py
+++ back.py
@@ -1,42 +1,43 @@
+field = [[0 for j in range(8)] for i in range(8)]
+white_king = '♔'
+white_queen = '♕'
+white_rook = '♖'
+white_bishop = '♗'
+white_knight = '♘'
+white_pawn = '♙'
+black_king = '♚'
+black_queen = '♚'
+black_rook = '♜'
+black_bishop = '♝'
+black_knight = '♞'
+black_pawn = '♟'
+for i in range(len(field[1])):
+    field[1][i] = black_pawn
+for i in range(len(field[6])):
+    field[6][i] = white_pawn
+
+field[0][0] = black_rook
+field[0][7] = black_rook
+field[7][0] = white_rook
+field[7][7] = white_rook
+
+field[0][1] = black_knight
+field[0][6] = black_knight
+field[7][1] = white_knight
+field[7][6] = white_knight
+
+field[0][2] = black_bishop
+field[0][5] = black_bishop
+field[7][2] = white_bishop
+field[7][5] = white_bishop
+
+field[0][3] = black_queen
+field[0][4] = black_king
+field[7][3] = white_queen
+field[7][4] = white_king
+
+
 def generate_field():
-    field = [[0 for j in range(8)] for i in range(8)]
-    white_king = '♔'
-    white_queen = '♕'
-    white_rook = '♖'
-    white_bishop = '♗'
-    white_knight = '♘'
-    white_pawn = '♙'
-    black_king = '♚'
-    black_queen = '♚'
-    black_rook = '♜'
-    black_bishop = '♝'
-    black_knight = '♞'
-    black_pawn = '♟'
-    for i in range(len(field[1])):
-        field[1][i] = black_pawn
-    for i in range(len(field[6])):
-        field[6][i] = white_pawn
-
-    field[0][0] = black_rook
-    field[0][7] = black_rook
-    field[7][0] = white_rook
-    field[7][7] = white_rook
-
-    field[0][1] = black_knight
-    field[0][6] = black_knight
-    field[7][1] = white_knight
-    field[7][6] = white_knight
-
-    field[0][2] = black_bishop
-    field[0][5] = black_bishop
-    field[7][2] = white_bishop
-    field[7][5] = white_bishop
-
-    field[0][3] = black_queen
-    field[0][4] = black_king
-    field[7][3] = white_queen
-    field[7][4] = white_king
-
     row_index = 8
     print(" ┌───┬───┬───┬───┬───┬───┬───┬───┐")
     for i in range(8):
--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-from back import *
 import os
+import time
+
+import back
+from back import *
 
 count = 0
 letter_dictionary = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4, "F": 5, "G": 6, "H": 7}
 
+
 def is_present(string: str) -> bool:
     if len(string) == 2:
         try:
-            return True
+            current_index = string[0:1]
+            current_row = 8 - int(string[1:2])
+            if 8 > current_row > 0:
+                figure = back.field[current_row][letter_dictionary[current_index]]
+                return True
+            else:
+                return False
         except IndexError:
+            return False
+        except ValueError:
+            return False
+        except KeyError:
             return False
     else:
         return False
 
+
 while True:
 
     if count != 0:
-        os.system("clear")
+        os.system("cls")
 
     generate_field()
     if count % 2 == 0:
@@ -29,9 +44,16 @@ while True:
         pass
     else:
         print("Такой фигуры не существует, попробуйте ещё раз!")
+        time.sleep(2)
+        count -= 1
         continue
+
     print("Сделайте ход: ")
     end = input()
-
-
-
+    if is_present(end):
+        pass
+    else:
+        print("Такого хода не существует, попробуйте ещё раз!")
+        time.sleep(2)
+        count -= 1
+        continue
